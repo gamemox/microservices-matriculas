9f668f252e11ff56d57e7a88c0baf1b91542cb74
gamemox <32118594+gamemox@users.noreply.github.com>
2017-10-25 00:57:12 -0300

    implementacion return

--- app/Http/Controllers/MatriculasController.php
+++ app/Http/Controllers/MatriculasController.php
@@ -9,15 +9,21 @@ class MatriculasController extends Controller
 {
 
       function getMatriculas(Request $request,$rut){
+        header("Access-Control-Allow-Origin: *");
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+        header('Access-Control-Allow-Credentials: true');  
+        
         $db = app('db');
         $results = $db->select("SELECT *
                                 FROM bdrutano
                                 where knumerut=".$rut);
         
-        return response()->json([
-            "contador" => count($results),
-            "datos" => $results,
-        ]);
+//        return response()->json([
+//            "contador" => count($results),
+//            "datos" => $results,
+//        ]);
+        return  json_encode($results);
        
    }
    
